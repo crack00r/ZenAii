ae6feb5cd83d2c1df9b872ec07d463f1c6b10e33
TheRoboKitten <jacobcmcqueen@gmail.com>
2017-08-17 22:02:30 -0500

    Update trader.py

--- trader.py
+++ trader.py
@@ -138,6 +138,96 @@ client.disconnect()
 
 
 
+def update_handler(d):
+    global flag
+    global variable
+    global var1
+    # On this example, we just show the update object itself
+    d = str(d)
+    #testChannel
+    re1 = '( id: )(?:[0-9][0-9]+)(,)' 
+
+    rg = re.compile(re1,re.IGNORECASE|re.DOTALL)
+    m = rg.search(d)
+    if m:
+        word1=m.group(0)
+        word2=word1.replace(' id: ', '')
+        word3=word2.replace(',', '')
+        word4=word3
+        idd = int(word4)
+        peer1 = InputPeerSelf()
+        #INPUT YOUR KEYWORDS BELOW
+        word_list = ["#DCR", "#LTC", "#NAUT", "#NXT", "#XCP", "#GRC", "#REP", "#PPC", "#RIC", "#STRAT", "#GAME", "#BTM", "#CLAM", "#ARDR", "#BLK", "#OMNI", "#SJCX", "#FLDC", "#BCH", "#DOGE", "#POT", "#VRC", "#ETH", "#PINK", "#NOTE", "#BTS", "#AMP", "#NAV", "#BELA", "#BCN", "#ETC", "#FLO", "#VIA", "#XBC", "#XPM", "#DASH", "#XVC", "#GNO", "#NMC", "#RADS", "#VTC", "#XEM", "#FCT", "#XRP", "#NXC", "#STEEM", "#SBD", "#BURST", "#XMR", "#DGB", "#LBC", "#BCY", "#PASC", "#SC", "#LSK", "#EXP", "#MAID", "#BTCD", "#SYS", "#GNT", "#HUC", "#EMC2", "#NEOS", "#ZEC", "#STR"]
+        regex_string = "(?<=\W)(%s)(?=\W)" % "|".join(word_list)
+        finder = re.compile(regex_string)
+        string_to_be_searched = d
+        results = finder.findall(" %s " % string_to_be_searched)
+        result_set = set(results)
+        print(idd)
+        for word in word_list:
+            if word in result_set:
+                try:
+                    var = word
+                    var1 = var.replace('#', '')
+                    btc = '-BTC'
+                    variable = var1 + btc
+                    m = create_process()
+                    m.start()
+                    client(ForwardMessageRequest(peer=peer1, id=(idd), random_id=(generate_random_long())))
+                except Exception as e:
+                    print(e)
+
+def create_process():
+    return multiprocessing.Process(target = runitt , args = ())
+ 
+def runitt():
+    global variable
+    global var1
+    variable=str(variable)
+    variablestr=str(variable)
+    print('Starting Buy Of: ' + variablestr + ' --  And will wait until bought')
+    process0='./zenbot.sh buy --order_adjust_time=10000 --debug  poloniex.' + variablestr	
+    proc0 = subprocess.Popen(process0,shell=True)
+    proc0.communicate()
+    if var1:
+       while True:
+            print('Taking first price measurement.. sell when price drops any over 30s')
+            wjdata1 = requests.get('https://poloniex.com/public?command=returnTicker&period=60').json()
+            for key in wjdata1:
+                if re.match(r'BTC_' + var1 + '+', key):
+                    print(key)       
+                    pct2=(wjdata1[key]['last'])
+                    pct3=Decimal(pct2)
+                    pr1=format(pct3, 'f')
+                    print(pr1)
+            time.sleep(30)
+            wjdata2 = requests.get('https://poloniex.com/public?command=returnTicker&period=60').json()
+            print('Taking second price measurement.. sell when price drops any over 30s')
+            for key in wjdata2:
+                if re.match(r'BTC_' + var1 + '+', key):
+                    print(key)       
+                    pct2=(wjdata2[key]['last'])
+                    pct3=Decimal(pct2)
+                    pr2=format(pct3, 'f')
+                    print(pr2)
+            if pr1 > pr2:
+                print('Starting Sell Of: ' + variablestr + ' --  Sell 100 pct at 0pct markup or manually sell using poloniex web interface... Be careful not to encounter a bug')
+                process1='./zenbot.sh sell --order_adjust_time=10000 --markup_pct=0 --debug  poloniex.' + variablestr	
+                proc1 = subprocess.Popen(process1,shell=True)
+                break
+            else:
+                print('Waiting for profit...')
+
+
+
+# From now on, any update received will be passed to 'update_handler'
+client.add_update_handler(update_handler)
+
+input('Press <ENTER> to exit...')
+client.disconnect()
+
+
+
     # On this example, we just show the update object itself
     d = str(d)
     #testChannel
